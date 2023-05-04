5e511c9be68dddb7d681e90c6c93c6bd61c05a1b
Rushikesh Kshirsagar <108231382+kshirsagarrushi@users.noreply.github.com>
2023-05-04 23:39:30 +0530

    footer changed

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -6,25 +6,47 @@ const Footer = () => {
     <footer>
       <div className={classes.wrapper}>
         <div className={classes.col}>
-          <h2>About the App</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Quisquam illum quam optio autem suscipit incidunt dicta dolorum eum dolores recusandae laboriosam expedita quo facilis, numquam et.
-            Delectus atque dolorum sapiente.
-          </p>
+            <div className="contact-info">
+              <h3>Contact Us</h3>
+              <p>123 Main St.</p>
+              <p>Anytown, USA 12345</p>
+              <p>Phone: 555-555-5555</p>
+              <p>Email: info@example.com</p>
+          </div>
         </div>
         <div className={classes.col}>
-          <h2>Contacts</h2>
-          <span>Phone: +123 456 789</span>
-          <span>YouTube: WebDevMania</span>
-          <span>GitHub: WebDevMania</span>
+          <div className="social-media">
+              <h3>Connect with us</h3>
+              <ul>
+                <li><a href="https://www.facebook.com/example"><i className="fab fa-facebook"></i>facebook</a></li>
+                <li><a href="https://www.twitter.com/example"><i className="fab fa-twitter"></i>twitter</a></li>
+                <li><a href="https://www.instagram.com/example"><i className="fab fa-instagram"></i>instagram</a></li>
+                <li><a href="https://www.linkedin.com/company/example"><i className="fab fa-linkedin"></i>linkedin</a></li>
+              </ul>
+          </div>
         </div>
         <div className={classes.col}>
-          <h2>Location</h2>
-          <span>Continent: Europe</span>
-          <span>Country: Bulgaria</span>
-          <span>Current Location: Bulgaria</span>
+          <div className="navigation">
+            <h3>Navigation</h3>
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/about">About Us</a></li>
+              <li><a href="/services">Services</a></li>
+              <li><a href="/properties">Properties</a></li>
+              <li><a href="/blog">Blog</a></li>
+              <li><a href="/contact">Contact Us</a></li>
+            </ul>
+          </div>
         </div>
+
+        <div className="legal">
+          <p>&copy; {new Date().getFullYear()} RentUp Real Estate. All rights reserved.</p>
+          <ul>
+            <li><a href="/privacy">Privacy Policy</a></li>
+            <li><a href="/terms">Terms of Use</a></li>
+          </ul>
+        </div>
+
       </div>
     </footer>
   )
