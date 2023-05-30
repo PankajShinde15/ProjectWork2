dfc14d6154ffb8383d47188fbefc6786e93389d7
PankajShinde15 <pankajvshinde15@gmail.com>
2023-05-30 16:36:43 +0530

    why to choose us

--- frontend/src/components/whyToChooseUs/whyUs.jsx
+++ frontend/src/components/whyToChooseUs/whyUs.jsx
@@ -11,28 +11,28 @@ const WhyChooseUs = () => {
                         <i className="fas fa-home"></i>
                     </div>
                     <h3>Extensive Property Listings</h3>
-                    {/* <p>We offer a vast selection of high-quality properties to suit your needs and preferences.</p> */}
+                    <p>We offer a vast selection of high-quality properties to suit your needs and preferences.</p>
                 </div>
                 <div className="feature">
                     <div className="feature-icon">
                         <i className="fas fa-handshake"></i>
                     </div>
                     <h3>Trusted Real Estate Agents</h3>
-                    {/* <p>Our team of experienced and reliable real estate agents is dedicated to providing personalized assistance throughout your property journey. We prioritize your satisfaction and ensure a smooth transaction.</p> */}
+                    <p>Our team of experienced and reliable real estate agents is dedicated to providing personalized assistance throughout your property journey.</p>
                 </div>
                 <div className="feature">
                     <div className="feature-icon">
                         <i className="fas fa-money-bill-wave"></i>
                     </div>
                     <h3>Competitive Pricing</h3>
-                    {/* <p>We offer competitive pricing options for both buyers and sellers. Our aim is to provide fair deals and value for your investment, making sure you get the most out of your real estate transactions.</p> */}
+                    <p>We offer competitive pricing options for both buyers and sellers.</p>
                 </div>
                 <div className="feature">
                     <div className="feature-icon">
                         <i className="fas fa-shield-alt"></i>
                     </div>
                     <h3>Secure and Confidential</h3>
-                    {/* <p>Your privacy and security are of utmost importance to us. We handle all transactions and information with strict confidentiality and use industry-standard security measures to protect your data.</p> */}
+                    <p>Your privacy and security are of utmost importance to us.</p>
                 </div>
             </div>
         </section>
